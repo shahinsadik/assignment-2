554b24689da8e9ce8780208a8935ad242e33bbf6
Shahin Sadik <shahinsadik@gmail.com>
2024-05-22 21:13:23 +0600

    product route

--- src/modules/products/product.route.ts
+++ src/modules/products/product.route.ts
@@ -4,12 +4,18 @@ import { productSchema, productUpdateSchema } from "./product.validation";
 import validateRequest from "../../utils/validateRequest";
 const router = express.Router();
 
-
-
-router.post("/",validateRequest(productSchema), productController.crateProduct);
+router.post(
+  "/",
+  validateRequest(productSchema),
+  productController.crateProduct
+);
 router.get("/", productController.getAllProduct);
 router.get("/:productId", productController.getProductById);
-router.put("/:productId",validateRequest(productUpdateSchema), productController.updateProductById);
+router.put(
+  "/:productId",
+  validateRequest(productUpdateSchema),
+  productController.updateProductById
+);
 router.delete("/:productId", productController.deleteProductById);
 router.get("/", productController.searchProducts);
 export const ProductRoutes = router;
