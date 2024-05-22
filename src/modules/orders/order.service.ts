839a0eee21a0adea07a74c242e0e1ea983dfb02f
Shahin Sadik <shahinsadik@gmail.com>
2024-05-22 21:18:12 +0600

    order service

--- src/modules/orders/order.service.ts
+++ src/modules/orders/order.service.ts
@@ -1,22 +1,22 @@
 import { TOrder } from "./order.interface";
 import { Order } from "./order.model";
 
-const createOrder= async (payLoad:TOrder)=>{
-
-    const result = await Order.create(payLoad);
-    return result;
-}
-const getAllOrder= async ()=>{
-
-    const result = await Order.find();
-    return result;
-}
+const createOrder = async (payLoad: TOrder) => {
+  const result = await Order.create(payLoad);
+  return result;
+};
+const getAllOrder = async () => {
+  const result = await Order.find();
+  return result;
+};
 
 const getOrdersByEmail = async (email: string) => {
-    const result = await Order.find({ email });
-    return result;
-}
+  const result = await Order.find({ email });
+  return result;
+};
 
-export const orderServices={
-    createOrder,getAllOrder,getOrdersByEmail,
-}
+export const orderServices = {
+  createOrder,
+  getAllOrder,
+  getOrdersByEmail,
+};
